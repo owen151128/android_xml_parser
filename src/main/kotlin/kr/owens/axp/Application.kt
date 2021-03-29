f67c09b3d8091aa7cb0f15344ae53c92f4ac95c6
owen151128 <owen151128@gmail.com>
2021-03-29 11:17:15 +0900

    Project default settings

---
+++ src/main/kotlin/kr/owens/axp/Application.kt
@@ -0,0 +1,13 @@
+package kr.owens.axp
+
+/**
+ * @author owen151128@everspin.co.kr
+ *
+ * Created by owen151128 on 2021/03/29 11:15
+ *
+ * Providing features related to Application class
+ */
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println(args.contentToString())
+}
